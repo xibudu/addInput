49a8a8ae59587e6877258588b3fcea9422f6fa60
xibudu <249143927@qq.com>
2016-04-26 10:56:39 +0800

    添加事件转化

--- js/app.js
+++ js/app.js
@@ -5,11 +5,12 @@ angular.module('myApp', ['ng'])
     $scope.add=function(){
       var obj={index:i++};
       $scope.lists.push(obj);
-    }
+      _hmt.push(['_trackEvent', "angularJS", "add", "item"]);
+    };
 
     $scope.del=function(idx){
       $scope.lists.splice(idx,1);
-    }
+    };
     $scope.submit = function () {
       $scope.lists.sort(function(a,b){
         return a.index- b.index;
@@ -17,7 +18,8 @@ angular.module('myApp', ['ng'])
       var json = {};
       $.each($scope.lists,function(i,v){
         json[v.key]= v.value;
-      })
+      });
       console.log(json);
+      _hmt.push(['_trackEvent', "angularJS", "submit", "server"]);
     }
-  })
+  });
